e96fea948003f2fc96f8d154449d8d593cd85544
LiuSu <liusumy@gmail.com>
2016-04-05 19:06:36 +0800

    Test commit 1

---
+++ nsprojectmain/src/main/java/com/liusu/nspmain/SecStore.java
@@ -0,0 +1,11 @@
+package com.liusu.nspmain;
+
+/**
+ * SecStore.java
+ * Created by liusu on 5/4/16.
+ */
+public class SecStore {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
