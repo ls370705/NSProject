916badc7e14c0b6a3360ef56062740dc8b2608ec
LiuSu <liusumy@gmail.com>
2016-04-05 21:32:59 +0800

    Test commit 2 Create client class

---
+++ nsprojectmain/src/main/java/com/liusu/nspmain/SecClient.java
@@ -0,0 +1,11 @@
+package com.liusu.nspmain;
+
+/**
+ * SecClient.java
+ * Created by liusu on 5/4/16.
+ */
+public class SecClient {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
